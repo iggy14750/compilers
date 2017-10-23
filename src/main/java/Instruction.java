f5b98d90bbc418002e4a7b899e7d318ad06920d0
Isaac <isaacgoss14750@gmail.com>
2017-10-22 20:30:35 -0400

    paramaterized instructions

--- src/main/java/Instruction.java
+++ src/main/java/Instruction.java
@@ -1,19 +1,7 @@
 
 
-public class Instruction {
-    private final int type;
-    private final int rs;
-    private final int rt;
-    private final int rd;
-
-    public Instruction(int type, int rd, int rs, int rt) {
-        this.type = type;
-        this.rs = rs;
-        this.rt = rt;
-        this.rd = rd;
-    }
-
-    public String toString() {
-        return String.format("add $r%d, $r%d, $r%d", rd, rs, rt);
-    }
+public interface Instruction {
+    public int opCode();
+    public String toString();
+    public String pack();
 }
