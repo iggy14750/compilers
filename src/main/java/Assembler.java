f3edff282aad28e57d48bac07e2fa1190194f73f
Isaac <isaacgoss14750@gmail.com>
2017-10-23 00:03:15 -0400

    final changes

--- src/main/java/Assembler.java
+++ src/main/java/Assembler.java
@@ -9,25 +9,6 @@ public class Assembler {
         MipsParse p = new MipsParse(new MipsLex(new FileReader(args[0])));
         List<Instruction> prog = (List<Instruction>) p.parse().value;
         for (Instruction inst: prog) {
-            String type = "";
-            if (inst instanceof RType) {
-                type = "rtype";
-            } else if (inst instanceof JType) {
-                type = "jtype";
-            } else if (inst instanceof IType) {
-                type = "itype";
-            } else if (inst instanceof LabeledInstruction) {
-                type = "label";
-            } else {
-                type = "UNKNOWN";
-            }
-            System.out.println(type + ": " + inst);
-        }
-
-        System.out.println("\n\n================================");
-        System.out.println("========= REAL OUTPUT ==========");
-        System.out.println("================================");
-        for (Instruction inst: prog) {
             System.out.println(inst.pack());
         }
     }
