41bc50c4ef2567a62aa37140795d8c7ef85d337e
Isaac <isaacgoss14750@gmail.com>
2017-11-26 17:35:12 -0500

    just finally added a file

---
+++ src/test/java/TestTreeVisitor.java
@@ -0,0 +1,25 @@
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.Assert;
+
+import syntaxtree.*;
+
+import frontend.Lexer;
+import frontend.Parser;
+
+import java.io.*;
+
+public class TestTreeVisitor {
+
+    private Program prog;
+
+    public TestTreeVisitor() throws Exception {
+        prog = TestRunner.parseFile("TreeVisitor.java");
+    }
+
+    @Test
+    public void exists() {
+        Assert.assertTrue(true);
+    }
+}
