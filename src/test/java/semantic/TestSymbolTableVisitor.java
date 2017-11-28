81a01adb12747a1fdac9962a5cbe99c1e626a980
Isaac <isaacgoss14750@gmail.com>
2017-11-28 14:17:52 -0500

    fully exploring the MainClass

---
+++ src/test/java/semantic/TestSymbolTableVisitor.java
@@ -0,0 +1,34 @@
+
+package semantic;
+
+import org.junit.*;
+import syntaxtree.*;
+
+public class TestSymbolTableVisitor {
+
+    private SymbolTableVisitor v = new SymbolTableVisitor();
+
+    private Program prog = new Program(
+        new MainClass(new Identifier("Main"), new Identifier("args"), new Print(new IntegerLiteral(5))),
+        new ClassDeclList()
+    );
+
+    @Test
+    public void callWithNullThenStop() {
+        v.visit((Program) null);
+    }
+
+    @Test
+    public void givenProgram() {
+        v.visit(prog);
+        Assert.assertEquals(Symbol.MAIN_CLASS, v.table.getSymbol("Main"));
+        SymbolTable clss = v.table.getChildScope("Main");
+        Assert.assertEquals(Symbol.METHOD, clss.getSymbol("main"));
+        Assert.assertEquals(SymbolType.VOID, clss.getSymbol("main").getMethodSignature().returnType);
+        Assert.assertArrayEquals(
+            new SymbolType[] {SymbolType.STRING_ARRAY},
+            clss.getSymbol("main").getMethodSignature().params
+        );
+    }
+
+}
