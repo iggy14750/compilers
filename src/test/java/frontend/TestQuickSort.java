d5c1013e90e57581d082a25805c7fa75a0d3de46
Isaac <isaacgoss14750@gmail.com>
2017-11-27 22:35:16 -0500

    position of class names

--- src/test/java/frontend/TestQuickSort.java
+++ src/test/java/frontend/TestQuickSort.java
@@ -13,14 +13,26 @@ import frontend.Parser;
 import java.io.*;
 
 public class TestQuickSort {
-    private Program prog;
 
-    public TestQuickSort() {
-        prog = TestRunner.parseFile("QuickSort.java");
+    private Program prog;
+    private Parser parser;
+
+    public TestQuickSort() throws Exception {
+        parser = new Parser(new File("src/test/etc/QuickSort.java"));
+        prog = parser.getProgram();
     }
 
     @Test
     public void exists() {
         Assert.assertTrue(true);
+    }
+
+    @Test
+    public void findClassId() {
+        Identifier name = ((ClassDeclSimple) prog.cl.elementAt(0)).i;
+        Position pos = parser.getPosition(name);
+        Assert.assertNotNull(pos);
+        Assert.assertEquals(11, pos.line);
+        Assert.assertEquals(7, pos.column);
     }
 }
