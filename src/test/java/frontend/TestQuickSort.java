412d7171aec53c0d570a7764f0143ca33b5ee1b0
Isaac <isaacgoss14750@gmail.com>
2017-11-27 23:03:52 -0500

    finding expression identifiers DEEEEP in the AST

--- src/test/java/frontend/TestQuickSort.java
+++ src/test/java/frontend/TestQuickSort.java
@@ -44,4 +44,21 @@ public class TestQuickSort {
         Assert.assertEquals(31, pos.line);
         Assert.assertEquals(16, pos.column);
     }
+
+    @Test
+    public void findExprName() {
+        MethodDecl method = ((ClassDeclSimple) prog
+            .cl.elementAt(0)) // Class list
+            .ml.elementAt(1); // Method list
+        Block block = (Block) ((If) method.sl.elementAt(1)).s1;
+        While outerWhile = (While) block.sl.elementAt(4);
+        While innerWhile = (While) ((Block) outerWhile.s).sl.elementAt(1);
+        If ifStatement = (If) ((Block) innerWhile.s).sl.elementAt(2);
+        IdentifierExp name = (IdentifierExp) ((LessThan) ((Not) ifStatement.e).e).e1;
+
+        Position pos = parser.getPosition(name);
+        Assert.assertNotNull(pos);
+        Assert.assertEquals(51, pos.line);
+        Assert.assertEquals(13, pos.column);
+    }
 }
