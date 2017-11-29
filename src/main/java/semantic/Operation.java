64fb32c2cd44b9465f0e0b7ae841a793705e798b
Isaac <isaacgoss14750@gmail.com>
2017-11-28 22:13:54 -0500

    basics of what I want to do are up!

---
+++ src/main/java/semantic/Operation.java
@@ -0,0 +1,22 @@
+
+package semantic;
+
+public enum Operation {
+    PLUS,
+    MINUS,
+    TIMES,
+    AND,
+    LESS_THAN,
+    NOT,
+    COPY,
+    GOTO,
+    IFFALSE,
+    PARAM,
+    CALL,
+    RETURN,
+    INDEX_FROM,
+    INDEX_TO,
+    NEW_TYPE,
+    NEW_OBJECT,
+    LENGTH
+}
---
+++ src/main/java/semantic/Quad.java
@@ -0,0 +1,17 @@
+
+package semantic;
+
+public class Quad {
+    
+    public final Operation op;
+    public final String result;
+    public final String operand1;
+    public final String operand2;
+
+    public Quad(Operation o, String op1, String op2, String res) {
+        op = o;
+        operand1 = op1;
+        operand2 = op2;
+        result = res;
+    }
+}
